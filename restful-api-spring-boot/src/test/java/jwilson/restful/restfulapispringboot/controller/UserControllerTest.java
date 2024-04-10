6322d0f945b76798d7adbc80255cdcb728b5e858
Joe Wilson <wilsonmangunsong@gmail.com>
2024-04-10 12:06:02 +0700

    finish-test-usercontrollertest

--- restful-api-spring-boot/src/test/java/jwilson/restful/restfulapispringboot/controller/UserControllerTest.java
+++ restful-api-spring-boot/src/test/java/jwilson/restful/restfulapispringboot/controller/UserControllerTest.java
@@ -14,9 +14,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import jwilson.restful.restfulapispringboot.entity.User;
 import jwilson.restful.restfulapispringboot.model.RegisterUserRequest;
 import jwilson.restful.restfulapispringboot.model.WebResponse;
 import jwilson.restful.restfulapispringboot.repository.UserRepository;
+import jwilson.restful.restfulapispringboot.security.BCrypt;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -55,6 +57,59 @@ class UserControllerTest {
           });
 
           assertEquals("OK", response.getData());
+    });
+  }
+
+  @Test
+  void testRegisterBadRequest() throws Exception {
+    RegisterUserRequest request = new RegisterUserRequest();
+    // empty the column for testing
+    request.setUsername("");
+    request.setPassword("");
+    request.setName("");
+
+    mockMvc.perform(
+            post("/api/users")
+              .accept(MediaType.APPLICATION_JSON)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(request))
+      ).andExpectAll(
+        status().isBadRequest()
+      ).andDo(result -> {
+          WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+          });
+
+          assertNotNull(response.getErrors());
+    });
+  }
+
+  @Test
+  void testRegisterDuplicate() throws Exception {
+    // create User first, then after this, we're goin to duplicate this data
+    User user = new User();
+    user.setUsername("test");
+    user.setPassword(BCrypt.hashpw("secret", BCrypt.gensalt()));
+    user.setName("Test");
+    userRepository.save(user);
+
+    RegisterUserRequest request = new RegisterUserRequest();
+    // empty the column for testing
+    request.setUsername("test");
+    request.setPassword("secret");
+    request.setName("Test");
+
+    mockMvc.perform(
+            post("/api/users")
+              .accept(MediaType.APPLICATION_JSON)
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(request))
+      ).andExpectAll(
+        status().isBadRequest()
+      ).andDo(result -> {
+          WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+          });
+
+          assertNotNull(response.getErrors());
     });
   }
 }
